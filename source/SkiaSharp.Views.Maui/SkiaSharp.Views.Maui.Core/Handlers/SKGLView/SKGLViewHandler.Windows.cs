fb10789422c16c8bd8f3af432338082b4e24cfdf
Matthew Leibowitz <mattleibow@live.com>
2023-09-05 00:52:39 +0200

    Implemented SKGLView for Android and iOS
    - Mac Catalyst will not get support - a new Metal-based view needs to be created
    - Tizen needs the support in the native view
    - Windows does not yet have GPU support

---
+++ source/SkiaSharp.Views.Maui/SkiaSharp.Views.Maui.Core/Handlers/SKGLView/SKGLViewHandler.Windows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Maui.Handlers;
+using SkiaSharp.Views.Maui.Platform;
+using SkiaSharp.Views.Windows;
+
+namespace SkiaSharp.Views.Maui.Handlers
+{
+	public partial class SKGLViewHandler : ViewHandler<ISKGLView, FrameworkElement>
+	{
+		private SKSizeI lastCanvasSize;
+		private SKTouchHandler? touchHandler;
+
+		protected override FrameworkElement CreatePlatformView() => throw new PlatformNotSupportedException("SKGLView is not yet implemented for WinUI.");
+
+		public static void MapIgnorePixelScaling(SKGLViewHandler handler, ISKGLView view) { }
+
+		public static void MapHasRenderLoop(SKGLViewHandler handler, ISKGLView view) { }
+
+		public static void MapEnableTouchEvents(SKGLViewHandler handler, ISKGLView view) { }
+
+		public static void OnInvalidateSurface(SKGLViewHandler handler, ISKGLView view, object? args) { }
+	}
+}
